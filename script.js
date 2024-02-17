8ff4532fd1a86d6b315a0b4e84e14c5c1541cb7a
codesoup <82330389+helikopterelidojosa@users.noreply.github.com>
2024-02-17 22:15:52 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+var movenumber =  10;
+
+
+function move1(){
+    document.getElementById("null1").style.display = "none";
+ 
+movenumber--;
+if(movenumber % 2 == 0 && movenumber > 0){
+    document.getElementById("td1").innerHTML = "O";
+}else{document.getElementById("td1").innerHTML = "X";}
+}
+
+function move2(){
+    document.getElementById("null2").style.display = "none";
+ 
+movenumber--;
+if(movenumber % 2 == 0 && movenumber > 0){
+    document.getElementById("td2").innerHTML = "O";
+}else{document.getElementById("td2").innerHTML = "X";}
+}
+
+function move3(){
+    document.getElementById("null3").style.display = "none";
+ 
+movenumber--;
+if(movenumber % 2 == 0 && movenumber > 0){
+    document.getElementById("td3").innerHTML = "O";
+}else{document.getElementById("td3").innerHTML = "X";}
+}
+
+function move4(){
+    document.getElementById("null4").style.display = "none";
+ 
+movenumber--;
+if(movenumber % 2 == 0 && movenumber > 0){
+    document.getElementById("td4").innerHTML = "O";
+}else{document.getElementById("td4").innerHTML = "X";}
+}
+
+function move5(){
+    document.getElementById("null5").style.display = "none";
+ 
+movenumber--;
+if(movenumber % 2 == 0 && movenumber > 0){
+    document.getElementById("td5").innerHTML = "O";
+}else{document.getElementById("td5").innerHTML = "X";}
+}
+
+function move6(){
+    document.getElementById("null6").style.display = "none";
+ 
+movenumber--;
+if(movenumber % 2 == 0 && movenumber > 0){
+    document.getElementById("td6").innerHTML = "O";
+}else{document.getElementById("td6").innerHTML = "X";}
+}
+
+function move7(){
+    document.getElementById("null7").style.display = "none";
+ 
+movenumber--;
+if(movenumber % 2 == 0 && movenumber > 0){
+    document.getElementById("td7").innerHTML = "O";
+}else{document.getElementById("td7").innerHTML = "X";}
+}
+
+function move8(){
+    document.getElementById("null8").style.display = "none";
+ 
+movenumber--;
+if(movenumber % 2 == 0 && movenumber > 0){
+    document.getElementById("td8").innerHTML = "O";
+}else{document.getElementById("td8").innerHTML = "X";}
+}
+
+function move9(){
+    document.getElementById("null9").style.display = "none";
+ 
+movenumber--;
+if(movenumber % 2 == 0 && movenumber > 0){
+    document.getElementById("td9").innerHTML = "O";
+}else{document.getElementById("td9").innerHTML = "X";}
+}
